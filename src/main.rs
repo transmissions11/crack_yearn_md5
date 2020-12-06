ce091f47705f7e71f97ac63eb638a24a01cedb1f
Transmissions <greenfeatherhelp@gmail.com>
2020-12-06 15:30:41 -0800

    Fixes but I think this is still wrong

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,42 @@ fn hash_md5(input: String) -> String {
 }
 
 fn main() {
-    let goal_hash = "dbba1bfe930d953cabcc03d7b6ab05e6";
+    let goal_hash = "dbba1bfe930d953cabcc03d7b6ab05e";
 
-    let alphabet = "bdeilmostu"
+    let chars = "bdeilmostu-";
+
+    let repeated_chars = chars.repeat(17);
+
+    let alphabet = repeated_chars
         .split("")
         .map(String::from)
         .filter(|letter| !letter.is_empty());
 
-    let words = alphabet.combinations_with_replacement(17).collect_vec();
+    let words = alphabet.combinations(17).collect_vec();
 
-    println!("Length of words: {}", words.len());
+    let length = words.len();
+    println!("Length of words: {}", length);
 
     thread::sleep(Duration::from_secs(2));
 
-    for word in words.iter() {
+    for (attempts, word) in words.iter().enumerate() {
         let joined: String = word.join("");
+
         let merged = format!(
             "{}{}",
             joined, "........................................................!1"
         );
 
-        println!("{}", merged);
+        let hash = hash_md5(hash_md5(hash_md5(hash_md5(merged.clone()))));
 
-        let hash = hash_md5(hash_md5(hash_md5(hash_md5(merged))));
+        println!(
+            "Attempts: {}/{}, Hash: {}, Text: {}",
+            attempts, length, hash, merged
+        );
 
         if hash == goal_hash {
             println!("We found it: {}", joined);
-            thread::sleep(Duration::from_secs(100));
+
             return;
         }
     }
